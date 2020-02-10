63d6279494c62ee27941c13253a3519203d5eef4
Κωνσταντίνος Λευθέρης <c.leftheris@indice.gr>
2020-02-10 13:37:00 +0200

    minor fix on using cloud configuration manager

--- src/Indice.Kentico.Oidc/OAuthConfiguration.cs
+++ src/Indice.Kentico.Oidc/OAuthConfiguration.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Configuration;
+using Microsoft.Azure;
 
 namespace Indice.Kentico.Oidc
 {
     public static class OAuthConfiguration
     {
-        public static string Authority => ConfigurationManager.AppSettings["Oidc:Authority"];
-        public static string Host => ConfigurationManager.AppSettings["Oidc:Host"];
-        public static string ClientId => ConfigurationManager.AppSettings["Oidc:ClientId"];
-        public static string ClientSecret => ConfigurationManager.AppSettings["Oidc:ClientSecret"];
-        public static string[] Scopes => ConfigurationManager.AppSettings["Oidc:Scopes"] != null ? ConfigurationManager.AppSettings["Oidc:Scopes"].Split(' ') : Array.Empty<string>();
+        public static string Authority => CloudConfigurationManager.GetSetting("Oidc:Authority");
+        public static string Host => CloudConfigurationManager.GetSetting("Oidc:Host");
+        public static string ClientId => CloudConfigurationManager.GetSetting("Oidc:ClientId");
+        public static string ClientSecret => CloudConfigurationManager.GetSetting("Oidc:ClientSecret");
+        public static string[] Scopes => CloudConfigurationManager.GetSetting("Oidc:Scopes")?.Split(' ') ?? Array.Empty<string>();
     }
 }
