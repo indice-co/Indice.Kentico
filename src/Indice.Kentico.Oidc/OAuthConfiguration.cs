8c206cad122ee3c164b617070097e100a84d5e98
Δημήτρης Χαραλαμπίδης <d.charalampidis@indice.gr>
2020-02-20 11:14:05 +0200

    Removed cloud configuration manager. Using System.Configuration https://stackoverflow.com/questions/35495505/azure-websites-complaining-role-environment-failed-to-initialize-on-startup

--- src/Indice.Kentico.Oidc/OAuthConfiguration.cs
+++ src/Indice.Kentico.Oidc/OAuthConfiguration.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.Azure;
 
 namespace Indice.Kentico.Oidc
 {
     public static class OAuthConfiguration
     {
-        public static bool AutoRedirect => bool.TryParse(CloudConfigurationManager.GetSetting("Oidc:AutoRedirect"), out var autoRedirect) ? autoRedirect : false;
-        public static string Authority => CloudConfigurationManager.GetSetting("Oidc:Authority");
-        public static string Host => CloudConfigurationManager.GetSetting("Oidc:Host");
-        public static string ClientId => CloudConfigurationManager.GetSetting("Oidc:ClientId");
-        public static string ClientSecret => CloudConfigurationManager.GetSetting("Oidc:ClientSecret");
-        public static string[] Scopes => CloudConfigurationManager.GetSetting("Oidc:Scopes")?.Split(' ') ?? Array.Empty<string>();
+        public static bool AutoRedirect => bool.TryParse(ConfigurationManager.AppSettings["Oidc:AutoRedirect"], out var autoRedirect) ? autoRedirect : false;
+        public static string Authority =>  ConfigurationManager.AppSettings["Oidc:Authority"];
+        public static string Host => ConfigurationManager.AppSettings["Oidc:Host"];
+        public static string ClientId => ConfigurationManager.AppSettings["Oidc:ClientId"];
+        public static string ClientSecret => ConfigurationManager.AppSettings["Oidc:ClientSecret"];
+        public static string[] Scopes => ConfigurationManager.AppSettings["Oidc:Scopes"]?.Split(' ') ?? Array.Empty<string>();
     }
 }
